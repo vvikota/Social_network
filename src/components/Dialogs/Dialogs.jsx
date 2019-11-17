8303de39419733001d9190b4054a95e679a24ee7
vvikota <rymsha.viktor@gmail.com>
2019-11-17 17:02:29 +0300

    add two column in dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,10 +3,21 @@ import classes from './Dialogs.module.scss';
 
 const Dialogs = (props) => {
   return (
-    <div >
-      Dialogs
+    <div className={classes.dialogs}>
+      <div className={classes.dialogsItems}>
+        <div className={classes.dialog  + ' ' + classes.active}>Vasja</div>
+        <div className={classes.dialog}>Petja</div>
+        <div className={classes.dialog}>Kostik</div>
+        <div className={classes.dialog}>Masha</div>
+        <div className={classes.dialog}>Nina</div>
+        <div className={classes.dialog}>Sveta</div>
+      </div>
+      <div className={classes.messages}>
+        <div className={classes.message}>Hi</div>
+        <div className={classes.message}>Hello! How about cinema</div>
+        <div className={classes.message}>I like cinema</div>
+      </div>
     </div>
- 
   )
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,8 @@ import classes from './Header.module.scss';
 const Header = () => {
   return (
     <header className={classes.header}>
-      <img src="https://mpng.pngfly.com/20180420/zpe/kisspng-kure-beach-logo-island-beach-5ad997fa00aa06.1930373715242096580027.jpg" alt="logo"/>
+      <img src="https://pbs.twimg.com/profile_images/844543325915025408/vNvUHv6T_400x400.jpg" alt="logo"/>
+      <h1>Social network</h1>
     </header>
   )
 }
