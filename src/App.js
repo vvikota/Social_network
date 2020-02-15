388cb781062eec34e44aee3842e2ec594bf9f260
vvikota <rymsha.viktor@gmail.com>
2020-02-15 22:02:43 +0300

    12 lesson

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header.jsx'
+import Navigation from './components/Navigation'
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div className='app-wrapper'>
+        <Header/>
+        <nav className='nav'>
+          <div>Profile</div>
+          <div>Messages</div>
+          <div>News</div>
+          <div>Music</div>
+          <div>Settings</div>
+        </nav>
+
+        <div className="content">
+          <img src="https://kogdakotika.net/media/post_images/title_page_m_ChtRYfI.jpg" alt="beach"/>
+        </div>
+        
+      </div>
+  )
+};
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+  return(
+  <header className='header'>
+    <img src="https://image.shutterstock.com/image-vector/vi-logo-initial-triangle-260nw-1202514451.jpg" alt="logo"/>
+  </header>)
+}
+
+export default Header
