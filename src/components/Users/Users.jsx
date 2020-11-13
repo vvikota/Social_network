092e794316ae089bbf94e52d5574fde119ec2b8d
vvikota <rymsha.viktor@gmail.com>
2020-11-13 15:00:39 +0300

    51 lesson: add axios

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,13 +1,26 @@
 import React from "react";
+import * as axios from "axios";
 import styles from './users.module.css';
+import userPhoto from '../../assets/images/user.png';
 
 const Users = (props) => {
-  return <>
+  if(props.users.length === 0) {
+    axios.get("https://social-network.samuraijs.com/api/1.0/users")
+      .then(response => {
+        props.setUsers(response.data.items)
+      })
+  }
+
+  return <section className={styles.users}>
     {
       props.users.map(user => <section key={user.id} className={styles.userCard}>
 
           <div className={styles.photoBlock}>
-            <img src={user.photoURL} alt="user-img" className={styles.photo} />
+            <img
+              src={user.photos.small === null ? userPhoto : user.photos.small}
+              alt="user-img"
+              className={styles.photo}
+            />
 
             <button
               className= { user.followed ? styles.unfollow : styles.follow}
@@ -20,18 +33,18 @@ const Users = (props) => {
 
           <div className={styles.userInformation}>
             <div>
-              <span className={styles.name}>{user.fullName}</span>
+              <span className={styles.name}>{user.name}</span>
               <span className={styles.status}>{user.status}</span>
             </div>
 
             <div>
-              <span className={styles.country}>{user.location.country}</span>
-              <span className={styles.city}>{user.location.city}</span>
+              <span className={styles.country}>{user.id}</span>
+              <span className={styles.city}>{user.uniqueUrlName}</span>
             </div>
           </div>
         </section>)
     }
-  </>
+  </section>
 }
 
 export default Users;
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -4,14 +4,15 @@ const TOGGLE_FOLLOW = 'TOGGLE_FOLLOW';
 const photoUrl = "https://i.pinimg.com/474x/a3/7f/b6/a37fb62e44a4ee7d9d0e04b79471018a.jpg";
 
 let initialState = {
-  users: [
-    {id: 1, photoURL: photoUrl, followed: true, fullName: 'Dmitriy', status: 'I am a boss', location: {city: 'Minsk', country: 'Belarus'}},
-    {id: 2,photoURL: photoUrl, followed: true, fullName: 'Viktor', status: 'I am a boss too', location: {city: 'Moscow', country: 'Russia'}},
-    {id: 3,photoURL: photoUrl, followed: false, fullName: 'Petr', status: 'cleaner', location: {city: 'Miory', country: 'Belarus'}},
-    {id: 4,photoURL: photoUrl, followed: false, fullName: 'Anastasiya', status: 'econoomist', location: {city: 'Minsk', country: 'Belarus'}},
-    {id: 5,photoURL: photoUrl, followed: true, fullName: 'olga', status: 'secretary', location: {city: 'Borisov', country: 'Belarus'}},
-    {id: 6,photoURL: photoUrl, followed: false, fullName: 'Vadim', status: 'driver', location: {city: 'Grodno', country: 'Belarus'}},
-  ]
+  // users: [
+  //   {id: 1, photoURL: photoUrl, followed: true, fullName: 'Dmitriy', status: 'I am a boss', location: {city: 'Minsk', country: 'Belarus'}},
+  //   {id: 2,photoURL: photoUrl, followed: true, fullName: 'Viktor', status: 'I am a boss too', location: {city: 'Moscow', country: 'Russia'}},
+  //   {id: 3,photoURL: photoUrl, followed: false, fullName: 'Petr', status: 'cleaner', location: {city: 'Miory', country: 'Belarus'}},
+  //   {id: 4,photoURL: photoUrl, followed: false, fullName: 'Anastasiya', status: 'econoomist', location: {city: 'Minsk', country: 'Belarus'}},
+  //   {id: 5,photoURL: photoUrl, followed: true, fullName: 'olga', status: 'secretary', location: {city: 'Borisov', country: 'Belarus'}},
+  //   {id: 6,photoURL: photoUrl, followed: false, fullName: 'Vadim', status: 'driver', location: {city: 'Grodno', country: 'Belarus'}},
+  // ]
+  users: []
 };
 
 const usersReducer = (state = initialState, action) => {
