5733a6296745fb2bd65c7cfd25c4a0cfb4fdd103
vvikota <rymsha.viktor@gmail.com>
2021-01-19 00:10:10 +0300

    81 lesson: add selectors for UsersContainer

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -3,23 +3,31 @@ import {
   setCurrentPage,
   toggleFollowAC,
   toggleFollowingProgress,
-  getUsers,
+  requestUsers,
   changeFollowed
 } from "../../redux/users-reducer";
 import React from "react";
 import Users from "./Users";
 import Preloader from "../common/Preloader/Preloader";
-import {withAuthRedirect} from "../../hoc/withAuthRedirect";
+// import {withAuthRedirect} from "../../hoc/withAuthRedirect";
 import {compose} from "redux";
+import {
+  getUsers,
+  getPageSize,
+  getTotalUsersCount,
+  getCurrentPage,
+  getIsFetching,
+  getFollowingInProgress
+} from "../../redux/users-selectors";
  
 class UsersContainer extends React.Component{
   componentDidMount() {
-    this.props.getUsers(this.props.currentPage, this.props.pageSize)
+    this.props.requestUsers(this.props.currentPage, this.props.pageSize)
   }
 
   onPageChanged = (pageNumber) => {
     this.props.setCurrentPage(pageNumber);
-    this.props.getUsers(pageNumber, this.props.pageSize)
+    this.props.requestUsers(pageNumber, this.props.pageSize)
   }
 
   render(){
@@ -41,16 +49,16 @@ class UsersContainer extends React.Component{
 }
 
 let MapStateToProps = (state) => ({
-  users: state.usersPage.users,
-  pageSize: state.usersPage.pageSize,
-  totalUsersCount: state.usersPage.totalUsersCount,
-  currentPage: state.usersPage.currentPage,
-  isFetching: state.usersPage.isFetching,
-  followingInProgress: state.usersPage.followingInProgress
+  users: getUsers(state),
+  pageSize: getPageSize(state),
+  totalUsersCount: getTotalUsersCount(state),
+  currentPage: getCurrentPage(state),
+  isFetching: getIsFetching(state),
+  followingInProgress: getFollowingInProgress(state),
 })
 
 export default compose(
   connect(MapStateToProps, {
-    toggleFollowAC, setCurrentPage, toggleFollowingProgress, getUsers, changeFollowed
+    toggleFollowAC, setCurrentPage, toggleFollowingProgress, requestUsers, changeFollowed
   })
 )(UsersContainer)
